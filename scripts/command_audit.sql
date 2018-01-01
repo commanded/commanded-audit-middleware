23f515189718a0e056e2fde0fa898d696d23c4f9
Ben Smith <ben@10consulting.com>
2018-01-01 21:47:20 +0000

    Include execution duration in ms and seconds in example SQL script

--- scripts/command_audit.sql
+++ scripts/command_audit.sql
@@ -9,6 +9,9 @@ select
   success,
   error,
   error_reason,
-  execution_duration_usecs
+  execution_duration_usecs,
+  execution_duration_usecs/1000 as execution_duration_ms,
+  execution_duration_usecs/1000000 as execution_duration_s
 from command_audit
-order by occurred_at;
+order by occurred_at DESC
+limit 50;
