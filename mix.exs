3e029962eedc6f7140f0881f2c704f6587d373cf
James Lavin <james@caredox.com>
2019-07-30 10:56:45 -0400

    bump version to  0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Commanded.Middleware.Auditing.Mixfile do
   def project do
     [
       app: :commanded_audit_middleware,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
