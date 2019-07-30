ccb3814d955dc5e6095624a613cc2f6d88c7d3e9
James Lavin <james@caredox.com>
2019-07-30 10:55:08 -0400

    run GIN index creation script only for JSONB columns

--- priv/repo/migrations/20190729144959_create_gin_index_on_commandstore_command_audit_data.exs
+++ priv/repo/migrations/20190729144959_create_gin_index_on_commandstore_command_audit_data.exs
@@ -2,10 +2,22 @@ defmodule Commanded.Middleware.Auditing.Repo.Migrations.CreateGinIndexOnCommands
   use Ecto.Migration
 
   def up do
-    execute "CREATE INDEX command_audit_data_gin ON command_audit USING GIN (data);"
+    if Application.get_env(:commanded_audit_middleware, :data_column_db_type, :bytea) == :jsonb do
+      execute(
+        "CREATE INDEX IF NOT EXISTS command_audit_data_gin ON command_audit USING GIN (data);"
+      )
+    end
+
+    if Application.get_env(:commanded_audit_middleware, :metadata_column_db_type, :bytea) ==
+         :jsonb do
+      execute(
+        "CREATE INDEX IF NOT EXISTS command_audit_metadata_gin ON command_audit USING GIN (metadata);"
+      )
+    end
   end
 
   def down do
-    execute "DROP INDEX command_audit_data_gin;"
+    execute("DROP INDEX IF EXISTS command_audit_metadata_gin;")
+    execute("DROP INDEX IF EXISTS command_audit_data_gin;")
   end
 end
